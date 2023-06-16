e09635ecb14b72e7c91092091d30478a3f86434f
Hari-810 <harijee098@gmail.com>
2023-06-16 12:04:54 +0530

    post, put, delete apiend points were created

--- books.py
+++ books.py
@@ -71,5 +71,74 @@ async def read_author_category_by_query(book_author: str, category: str):
  
 # here book_author was a path parameter and category was a query parameter
 
+#-----------------------------------------------------------------------------------------------------------
+#--------------------------------------   POST Request   --------------------------------------  
+
+from fastapi import FastAPI, Body
+# POST method requires body and GET doesn't needs that
 
 
+@app.post("/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    
+    
+# run the server give body content as dict of values
+#    ex:   {"title": "Title Seven", "author": "Author Six", "category": "History"}  always use ""
+
+#-----------------------------------------------------------------------------------------------------------
+#--------------------------------------   PUT Request   --------------------------------------  
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+            
+#-----------------------------------------------------------------------------------------------------------
+#--------------------------------------   DELETE Request   --------------------------------------  
+
+
+@app.delete("/books/delete_book/{book_title}")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
+
+#-----------------------------------------------------------------------------------------------------------
+#--------------------------------------  Assignment  --------------------------------------  
+
+
+    """
+    Assignment
+    
+    1. Create a new API Endpoint that can fetch all books from a specific author 
+    using either Path Parameters or Query Parameters.
+    """
+ 
+ 
+# Query parameter    
+@app.get("/author_books_query")
+async def author_books_query(author):
+    author_books = []
+    for book in BOOKS:
+        if book.get("author") == author:
+            author_books.append(book)
+    if len(author_books) == 0:
+        return "No Books were founded"
+    else:
+        return author_books
+    
+    
+@app.get("/author_books_path/{autor}")
+async def author_books_path(author):
+    author_books = []
+    for book in BOOKS:
+        if book.get("author") == author:
+            author_books.append(book)
+    if len(author_books) == 0:
+        return "No Books were founded"
+    else:
+        return author_books
+    
